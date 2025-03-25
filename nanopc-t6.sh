1dbbfc2f8ebdecd79233d65ceaa210268e937af4
hza1128 <141570215+hza1128@users.noreply.github.com>
2025-03-25 20:15:41 +0800

    Update nanopc-t6.sh

--- nanopc-t6.sh
+++ nanopc-t6.sh
@@ -11,10 +11,10 @@
 #
 
 # 设置默认ip
-sed -i 's/192.168.1.1/192.168.10.12/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.11/g' package/base-files/files/bin/config_generate
 
 # R4S机型调整网口,wan/lan对调，以适配T4机型
-sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
+#sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 
 # 设置默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argone/g' feeds/luci/collections/luci/Makefile
