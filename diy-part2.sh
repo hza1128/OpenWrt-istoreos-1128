58d9a9720f10c01cdbf1cc3146aa2e541257737c
hza1128 <141570215+hza1128@users.noreply.github.com>
2025-04-06 22:04:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,7 +17,9 @@ sed -i 's/192.168.1.1/192.168.10.11/g' package/base-files/files/bin/config_gener
 sed -i 's/luci-theme-bootstrap/luci-theme-argone/g' feeds/luci/collections/luci/Makefile
 
 # 修改主题背景
-cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+cp -f $GITHUB_WORKSPACE/bg1.jpg feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+
+cp -f $GITHUB_WORKSPACE/Makefile feeds/small/v2ray-plugin/Makefile
 
 # Modify hostname
 # sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
